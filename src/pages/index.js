6a47bb30f3c8915f2b796e6c91b358b04bd6a855
Laila ElMANSOUR <lailaelmanssour@gmail.com>
2019-07-08 19:15:03 +0100

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import styled from "styled-components"
+const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+const Title = styled.h1`
+  margin: 0;
+`
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    <Title>Coming soon...</Title>
+  </Layout>
+)
